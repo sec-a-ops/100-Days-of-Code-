4679c721bf1070540e82147536cde81c5dea053a
sec-a-ops <155834969+sec-a-ops@users.noreply.github.com>
2025-10-04 17:04:00 +0530

    Create Q4.c

---
+++ Q4.c
@@ -0,0 +1,29 @@
+//Q4: Write a program to calculate the area and circumference of a circle given its radius.
+
+/*
+Sample Test Cases:
+Input 1:
+7
+Output 1:
+Area=153.94, Circumference=43.96
+
+Input 2:
+3
+Output 2:
+Area=28.27, Circumference=18.85
+
+*/
+#include <stdio.h>
+#include <conio.h>
+#define PI 3.14
+
+int main() {
+    float radius, area, circumference;
+    printf("Enter radius of the circle: ");
+    scanf("%f", &radius);
+    area = PI * radius * radius;
+    circumference = 2 * PI * radius;
+    printf("Area=%.2f, Circumference=%.2f\n", area, circumference);
+    getch();
+    return 0;
+}
