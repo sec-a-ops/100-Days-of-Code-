d304b196689f17b7d3e2f1ce3e7244b4cad3f44b
sec-a-ops <155834969+sec-a-ops@users.noreply.github.com>
2025-10-24 13:18:30 +0530

    Create Q34.c

---
+++ Q34.c
@@ -0,0 +1,39 @@
+/*Q34: Write a program to check if a number is prime.
+
+/*
+Sample Test Cases:
+Input 1:
+7
+Output 1:
+Prime
+
+Input 2:
+10
+Output 2:
+Not prime
+
+*/
+#include <stdio.h>
+
+int main() {
+    int num, isPrime = 1;
+    scanf("%d", &num);
+
+    if (num <= 1) {
+        isPrime = 0;
+    } else {
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) {
+                isPrime = 0;
+                break;
+            }
+        }
+    }
+
+    if (isPrime)
+        printf("Prime\n");
+    else
+        printf("Not prime\n");
+
+    return 0;
+}
