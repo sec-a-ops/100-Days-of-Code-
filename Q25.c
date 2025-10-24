6e143d2c81ba1331582b99ff240f937a86b49502
sec-a-ops <155834969+sec-a-ops@users.noreply.github.com>
2025-10-24 13:02:32 +0530

    Create Q25.c

---
+++ Q25.c
@@ -0,0 +1,34 @@
+/*Q25: Write a program to implement a basic calculator using switch-case for +, -, *, /, %.
+/* Sample Test Cases: Input 1: 4 2 + Output 1: 6
+Input 2: 10 3 % Output 2: 1
+Input 3: 15 5 / Output 3: 3
+*/
+
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    char op;
+    scanf("%d %d %c", &a, &b, &op);
+
+    switch(op) {
+        case '+': printf("%d\n", a + b); break;
+        case '-': printf("%d\n", a - b); break;
+        case '*': printf("%d\n", a * b); break;
+        case '/': 
+            if (b != 0)
+                printf("%d\n", a / b);
+            else
+                printf("Division by zero error\n");
+            break;
+        case '%': 
+            if (b != 0)
+                printf("%d\n", a % b);
+            else
+                printf("Modulo by zero error\n");
+            break;
+        default: printf("Invalid operator\n");
+    }
+
+    return 0;
+}
