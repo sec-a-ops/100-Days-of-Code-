400d1a8b52497540662cea43374f4a9600703e53
sec-a-ops <155834969+sec-a-ops@users.noreply.github.com>
2025-10-28 16:38:06 +0530

    Create Q70.c

---
+++ Q70.c
@@ -0,0 +1,37 @@
+//Q70: Rotate an array to the right by k positions.
+
+/*
+Sample Test Cases:
+Input 1:
+5
+1 2 3 4 5
+2
+Output 1:
+4 5 1 2 3
+
+*/
+#include <stdio.h>
+int main() {
+    int n, k;
+    printf ("Enter the size of the array:\n");
+    scanf("%d", &n);   
+    int arr[n];
+    printf ("Enter the elements in array:\n");
+    for (int i = 0; i < n; i++)
+    {
+        printf ("a[%d] ",i);
+        scanf("%d", &arr[i]);   
+    }
+    printf ("Enter number of rotations:\n");
+    scanf("%d", &k);   
+    k = k % n;       
+    for (int i = n - k; i < n; i++)
+        {
+            printf("%d ", arr[i]);
+    }
+    for (int i = 0; i < n - k; i++)
+    {
+    printf("%d ", arr[i]);
+}
+    return 0;
+}
