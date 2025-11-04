cac22c23114ee459b6ed8a2843636719a389a6e6
sec-a-ops <155834969+sec-a-ops@users.noreply.github.com>
2025-11-04 09:27:27 +0530

    Create Q88.c

---
+++ Q88.c
@@ -0,0 +1,22 @@
+/*
+Ques: Replace spaces with hyphens in a string.
+*/
+
+#include <stdio.h>
+
+int main() {
+    char str[200];
+    int i;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    for (i = 0; str[i] != '\0'; i++) {
+        if (str[i] == ' ') {
+            str[i] = '-';
+        }
+    }
+
+    printf("Modified string: %s", str);
+    return 0;
+}
