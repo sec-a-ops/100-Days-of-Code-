9d918d00b3db99f10555e55e928c1106fdfe8110
sec-a-ops <155834969+sec-a-ops@users.noreply.github.com>
2025-11-04 09:20:29 +0530

    Create Q82.c

---
+++ Q82.c
@@ -0,0 +1,17 @@
+/*
+Print each character of a string on a new line.
+*/
+
+#include <stdio.h>
+
+int main() {
+    char str[1000];
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    for (int i = 0; str[i] != '\0' && str[i] != '\n'; i++) {
+        printf("%c\n", str[i]);
+    }
+
+    return 0;
+}
