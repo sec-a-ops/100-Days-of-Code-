fd148d0c8e2c1b9cb08b4bcd623aff701d9dbb5c
sec-a-ops <155834969+sec-a-ops@users.noreply.github.com>
2025-11-05 16:25:27 +0530

    Create Q112.c

---
+++ Q112.c
@@ -0,0 +1,42 @@
+//Q112: Write a program to take an integer array arr as input. The task is to find the maximum sum of any contiguous subarray using Kadane's algorithm. Print the maximum sum as output. If all elements are negative, print the largest (least negative) element.
+
+/*
+Sample Test Cases:
+Input 1:
+arr[] = [2, 3, -8, 7, -1, 2, 3]
+Output 1:
+11
+
+Input 2:
+arr[] = [-2, -4]
+Output 2:
+-2
+
+Input 3:
+arr[] = [5, 4, 1, 7, 8]
+Output 3:
+25
+
+*/
+#include <stdio.h>
+
+int main() {
+    int arr[] = {2, 3, -8, 7, -1, 2, 3};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int maxSum = arr[0], currentSum = arr[0], i;
+
+    for (i = 1; i < n; i++) {
+        if (currentSum < 0) {
+            currentSum = arr[i];
+        } else {
+            currentSum += arr[i];
+        }
+
+        if (currentSum > maxSum) {
+            maxSum = currentSum;
+        }
+    }
+
+    printf("%d\n", maxSum);
+    return 0;
+}
